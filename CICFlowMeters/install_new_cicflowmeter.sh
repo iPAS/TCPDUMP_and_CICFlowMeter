82e9811f3fe7844bdeec0b0b56051043ac645a33
iPAS <ptiwatthanont@gmail.com>
2018-05-15 12:33:52 +0700

    Revise the install_new_cicflowmeter.sh.

--- CICFlowMeters/install_new_cicflowmeter.sh
+++ CICFlowMeters/install_new_cicflowmeter.sh
@@ -1,8 +1,17 @@
 #!/bin/bash
 
 rm -rf CICFlowMeter-3.0
+
+[[ ! -d CICFlowMeter_repo ]] && (\
+    git clone https://github.com/iPAS/CICFlowMeter.git CICFlowMeter_repo || (\
+        echo "Cannot clone the repository https://github.com/iPAS/CICFlowMeter.git!" && exit 255))
+
+cd CICFlowMeter_repo
+gradle clean
+gradle build
+cd ..
+
 cp CICFlowMeter_repo/build/distributions/CICFlowMeter-3.0.zip .
 unzip CICFlowMeter-3.0.zip
 rm -f CICFlowMeter-3.0.zip
-cp CICFlowMeter.revised CICFlowMeter-3.0/bin/CICFlowMeter 
-
+cp -f CICFlowMeter.revised CICFlowMeter-3.0/bin/CICFlowMeter
