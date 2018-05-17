e520db6c488f0971d411963e837aa8be57fcd94a
yru <yru@local>
2018-05-17 14:01:31 +0700

    Convert and remove PCAP.

--- convert_pcap_csv.sh
+++ convert_pcap_csv.sh
@@ -19,19 +19,23 @@ mv "${pcap_file}" "${pcap_dir}"
 pcap_file="${pcap_dir}"/$(basename "${pcap_file}")
 
 echo '---'
+echo "pcap_dir:" "${pcap_dir}"
 ls "${pcap_dir}"
 echo '---'
 
 # CICFlowMeterV2
-cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV2
-java -Djava.library.path="${cic_dir}" -jar "${cic_dir}"/CICFlowMeter.jar "${pcap_dir}/" "${output_dir}/2/"
+#cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV2
+#java -Djava.library.path="${cic_dir}" -jar "${cic_dir}"/CICFlowMeter.jar "${pcap_dir}/" "${output_dir}/2/"
+
+#cic_dir=${HOME}/CICFlowMeterV2
+#java -Djava.library.path="${cic_dir}/jnetpcap" -jar "${cic_dir}"/CICFlowMeter.jar "${pcap_dir}/" "${output_dir}/2/"
 
 # CICFlowMeterV3-jnetpcap-1.3
-cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV3-jnetpcap-1.3
+#cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV3-jnetpcap-1.3
 #java -Djava.library.path="${cic_dir}" -jar "${cic_dir}"/CICFlowMeterV3.jar "${pcap_file}" "${output_dir}/3.1.3/"
 
 # CICFlowMeterV3-jnetpcap-1.4
-cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV3-jnetpcap-1.4
+#cic_dir="${script_dir}"/CICFlowMeters/CICFlowMeterV3-jnetpcap-1.4
 #java -Djava.library.path="${cic_dir}" -jar "${cic_dir}"/CICFlowMeterV3.jar "${pcap_file}" "${output_dir}/3.1.4/"
 
 
